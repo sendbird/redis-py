3ec7472213e67f4de53a2b1ab0ccc1f4c96cad0c
bellatoris <trollbellator@gmail.com>
2023-07-14 15:21:22 +0900

    fix lint

--- redis/connection.py
+++ redis/connection.py
@@ -879,9 +879,9 @@ class Connection:
             args = tuple(args[0].split()) + args[1:]
 
         # `COMMAND GETKEYS` can crash redis server entirely under certain conditions.
-        # So we have decided to make sure that `COMMAND GETKEYS` is never sent to the server.
-        # If you need to send `COMMAND GETKEYS` to the server, please reach out to Doogie
-        # and Zach to discuss the use case.
+        # So we have decided to make sure that `COMMAND GETKEYS` is never sent to the
+        # server. If you need to send `COMMAND GETKEYS` to the server, please reach out
+        # to Doogie and Zach to discuss the use case.
         # ref: https://github.com/redis/redis/pull/12380
         if len(args) > 1 and args[0].lower() == b'command' and args[1].lower().startswith(b'getkeys'):
             raise Exception(f'Redis command "{args[0].decode()} {args[1].decode()}" is not supported')
