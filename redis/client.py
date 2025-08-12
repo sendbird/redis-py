a55e5d5168fef38b990ffce46cd334ff4d10bb64
Doogie Min <trollbellator@gmail.com>
2025-08-12 10:26:10 +0900

    ssubscribe to keys separately by slot (#28)

--- redis/client.py
+++ redis/client.py
@@ -2,6 +2,7 @@ import copy
 import re
 import threading
 import time
+from collections import defaultdict
 from itertools import chain
 from typing import (
     TYPE_CHECKING,
@@ -39,6 +40,7 @@ from redis.connection import (
     SSLConnection,
     UnixDomainSocketConnection,
 )
+from redis.crc import key_slot
 from redis.credentials import CredentialProvider
 from redis.event import (
     AfterPooledConnectionsInstantiationEvent,
@@ -892,11 +894,14 @@ class PubSub:
             }
             self.psubscribe(**patterns)
         if self.shard_channels:
-            shard_channels = {
-                self.encoder.decode(k, force=True): v
-                for k, v in self.shard_channels.items()
-            }
-            self.ssubscribe(**shard_channels)
+            channels_by_slot = defaultdict(dict)
+            for k, v in self.shard_channels.items():
+                key = self.encoder.decode(k, force=True)
+                slot = key_slot(self.encoder.encode(key))
+                channels_by_slot[slot][key] = v
+
+            for slot, channels in channels_by_slot.items():
+                self.ssubscribe(**channels)
 
     @property
     def subscribed(self) -> bool:
@@ -1126,8 +1131,8 @@ class PubSub:
             args = list_or_args(args[0], args[1:])
         new_s_channels = dict.fromkeys(args)
         new_s_channels.update(kwargs)
-        for channel in new_s_channels:  # We should send ssubscribe one by one on redis cluster to prevent CROSSSLOT error
-            self.execute_command("SSUBSCRIBE", channel)
+        ret_val = self.execute_command("SSUBSCRIBE", *new_s_channels.keys())
+
         # update the s_channels dict AFTER we send the command. we don't want to
         # subscribe twice to these channels, once for the command and again
         # for the reconnection.
@@ -1139,6 +1144,7 @@ class PubSub:
             # Clear the health check counter
             self.health_check_response_counter = 0
         self.pending_unsubscribe_shard_channels.difference_update(new_s_channels)
+        return ret_val
 
     def sunsubscribe(self, *args, target_node=None):
         """
